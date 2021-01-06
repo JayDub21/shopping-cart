cd7a01ab787c322210f9eb8fe75a83df7bd929c2
JayDub21 <justin.wofford.21@gmail.com>
2021-01-06 03:09:45 -0600

    created onIncrement to make counter a controlled component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,21 +1,12 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value,
-  };
-
-  handleIncrement = (product) => {
-    this.setState({ value: this.state.value + 1 });
-    console.log(this.state.value, product);
-  };
-
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement(this.props.counter.id)}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className='btn btn-secondary btn-sm'
         >
           Increment
@@ -32,12 +23,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = 'badge m-2 badge-';
-    classes += this.state.value === 0 ? 'warning' : 'primary';
+    classes += this.props.counter.value === 0 ? 'warning' : 'primary';
     return classes;
   }
 
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? 'Zero' : value;
   }
 }
